c67e8851e463a6ff4c9ee94e8d549d08c59afaee
miketheshadow <miketheshadow1@gmail.com>
2021-01-06 03:16:44 -0700

    Initial commit

---
+++ src/main/java/com/miketheshadow/economymonitor/EconomyMonitor.java
@@ -0,0 +1,18 @@
+package com.miketheshadow.economymonitor;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EconomyMonitor extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/miketheshadow/economymonitor/MoneyMovementMonitor.java
@@ -0,0 +1,14 @@
+package com.miketheshadow.economymonitor;
+
+import org.bukkit.OfflinePlayer;
+
+public class MoneyMovementMonitor {
+
+    public static void playerSpentMoney(OfflinePlayer player, double amount) {
+
+    }
+
+    public static void playerReceivedMoney(OfflinePlayer player, double amount) {
+
+    }
+}
